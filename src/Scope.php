e4efa99051a94a29f6c6bbeae31c2ec1aadac4e1
Luttje <2738114+luttje@users.noreply.github.com>
2022-06-10 09:06:14 +0200

    Simplify dom changes to help livewire Note: still bugged, but now with less juggling of dom elements. The workaround is a 100% more hacky and causes recursion (because initialized script elements are re-executed, and the script element calls initialize() on the component)

--- src/Scope.php
+++ src/Scope.php
@@ -4,55 +4,79 @@ namespace Luttje\ScopedComponents;
 
 class Scope
 {
+    private static $currentScriptId = 0;
+
     /**
      * Injects PHP that starts a new scope.
      */
     public static function getStartScope()
     {
-        return <<<SCRIPT_ECHO
-        <div>
-            <?php
-            \$tagConfigs = [
-                [
-                    'tag' => 'noscript'
-                ],
-                [
-                    'tag' => 'template',
-                    'style' => 'display:none'
-                ],
-            ];
-            foreach (\$tagConfigs as \$tagConfig) {
-                extract(\$tagConfig);
+        // Used to generate a random id for reference later
+        $currentScriptId = ++self::$currentScriptId;
 
-                if(isset(\$style))
-                    echo "<\$tag style=\"\$style\">";
-                else
-                    echo "<\$tag>";
-            ?>
-        SCRIPT_ECHO;
+        return <<<SCRIPT_ECHO
+            <div>
+                <?php
+                \$tagConfigs = [
+                    //[
+                    //   'tag' => 'noscript'
+                    //],
+                    [
+                        'tag' => 'template',
+                        'attributes' => [
+                            'style' => 'display:none',
+                            'id' => 'scoped-element-$currentScriptId',
+                        ]
+                    ],
+                ];
+                foreach (\$tagConfigs as \$k => \$tagConfig) {
+                    extract(\$tagConfig);
+
+                    echo "<\$tag";
+
+                    if(isset(\$attributes)) {
+                        foreach (\$attributes as \$attribute => \$value) {
+                            echo " \$attribute=\"\$value\"";
+                        }
+                    }
+
+                    echo ">";
+                ?>
+            SCRIPT_ECHO;
     }
 
     public static function getEndScope()
     {
+        $currentScriptId = self::$currentScriptId;
+
         return <<<SCRIPT_ECHO
             <?php
                 echo "</\$tag>";
             } ?>
 
-            <script>
-            (function(thisScript) {
-                const templateEl = thisScript.previousElementSibling;
-                const parentEl = templateEl.parentNode;
-                const content = templateEl.content.cloneNode(true);
+            <script data-reexecute-on-livewire-update>
+                (function(cacheBreaker) {
+                    const templateEl = document.getElementById('scoped-element-$currentScriptId');
+                    const parentEl = templateEl.parentNode;
+                    const content = templateEl.content.cloneNode(true);
 
-                parentEl.innerHTML = '';
-                const shadow = parentEl.shadowRoot || parentEl.attachShadow({ mode:"open" });
-                shadow.append(content);
+                    const shadow = parentEl.shadowRoot || parentEl.attachShadow({ mode:"open" });
+                    shadow.innerHTML = '';
+                    shadow.append(content);
 
-                document.addEventListener('alpine:init', () => {
-                    Alpine.initTree(shadow);
-                });
-            })(document.currentScript);
+                    let comp = templateEl.closest('[wire\\\\3A id]');
+                    if(comp !== null && comp.__livewire !== undefined) {
+                        console.log(comp.__livewire);
+                        comp.__livewire.initialize();
+                    }
+
+                    if(typeof Alpine !== 'undefined')
+                        Alpine.initTree(shadow);
+                    else
+                        document.addEventListener('alpine:init', () => {
+                            Alpine.initTree(shadow);
+                        });
+                })(<?php echo time(); ?>);
             </script>
         </div>
         SCRIPT_ECHO;
